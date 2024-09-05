d4b9df945a0d27c810cf0a2484fafe38d57a5013
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-05 15:04:03 -0400

    init files and variable assignment

---
+++ SimpleStatistics.java
@@ -0,0 +1,14 @@
+package SimpleStatistics;
+
+import java.util.Scanner;
+
+public class SimpleStatistics {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int num1;
+      int num2;
+      int num3;
+      int num4;
+      
+   }
+}
