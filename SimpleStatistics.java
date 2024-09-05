6a325f1856d2b59232c4fce8002ade2336eee4b2
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-05 15:29:31 -0400

    assigned variables to input integers, assigned product and avg to int, printing results and closing scanner.

--- SimpleStatistics.java
+++ SimpleStatistics.java
@@ -5,10 +5,17 @@ import java.util.Scanner;
 public class SimpleStatistics {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
-      int num1;
-      int num2;
-      int num3;
-      int num4;
+      int num1 = scnr.nextInt();
+      int num2 = scnr.nextInt();
+      int num3 = scnr.nextInt();
+      int num4 = scnr.nextInt();
       
+      double product = num1 * num2 * num3 * num4;
+      double average = (num1 + num2 + num3 + num4) / 4;
+      
+      System.out.println(product);
+      System.out.println(average);
+
+      scnr.close();
    }
 }
